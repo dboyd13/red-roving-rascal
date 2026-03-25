b029253d85031f52a9aa67493befa68d93582a7b
Darran Boyd <drboyd@example.com>
2026-03-25 18:07:33 +1100

    Fix add_method API: pass authorization_type directly

--- src/rascal/cdk/construct.py
+++ src/rascal/cdk/construct.py
@@ -153,7 +153,7 @@ class RascalBackendConstruct(Construct):
 
         api.root.add_resource("health").add_method(
             "GET", nlb_integration("GET", "/health"),
-            options=apigw.MethodOptions(authorization_type=apigw.AuthorizationType.NONE),
+            authorization_type=apigw.AuthorizationType.NONE,
         )
 
         self.api_endpoint = api.url
